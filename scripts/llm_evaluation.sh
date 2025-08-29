2e1c920b847f78672d67ddcb8c830f591fc46644
leo-liuzy <zliu@cs.utexas.edu>
2025-08-29 03:49:13 +0000

    eval for meta-aug CPT

--- scripts/llm_evaluation.sh
+++ scripts/llm_evaluation.sh
@@ -14,9 +14,9 @@ rr=0.0
 
 test_set_pairs=(
     "ctrl_RE_id test_id_sample"
-    "ctrl_RE_ood_both test_ood_both_sample"
-    "ctrl_RE_ood_entity test_ood_entity_sample"
-    "ctrl_RE_ood_relation test_ood_relation_sample"
+    # "ctrl_RE_ood_both test_ood_both_sample"
+    # "ctrl_RE_ood_entity test_ood_entity_sample"
+    # "ctrl_RE_ood_relation test_ood_relation_sample"
 )
 
 while [[ $# -gt 0 ]]; do
@@ -45,7 +45,7 @@ eval_data_name="controlled_RE_efficacy"
 for pair in "${test_set_pairs[@]}"; do
     read -r task_name test_set_choice <<< "$pair"
     model_name_or_path=ckpts/${task_name}-${cpt_data_choice}-lr1e-05-rr${rr}-epochs8-bs16-wd0.01-constant-warmup0.05-Qwen2.5-1.5B-Instruct
-    if [ "$test_set_choice" != "ctrl_RE_id" ]; then
+    if [ "$task_name" != "ctrl_RE_id" ]; then
         python query_vllm.py --model-name-or-path $model_name_or_path --eval-data-name controlled_RE_efficacy --max-tokens 1024 --test-set-choice $test_set_choice # --overwrite
     else
         for eval_data_name in "all"; do
